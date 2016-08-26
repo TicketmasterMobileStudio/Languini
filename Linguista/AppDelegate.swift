c9ffc6d69b5d98cc55c451345f77bcb5c9bb082c
Chris Stroud <chris.stroud@ticketmaster.com>
2016-08-26 11:19:33 -0400

    Initial Commit

---
+++ Linguista/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Linguista
+//
+//  Created by Chris Stroud on 8/26/16.
+//  Copyright © 2016 Ticketmaster. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Linguista/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Linguista
+//
+//  Created by Chris Stroud on 8/26/16.
+//  Copyright © 2016 Ticketmaster. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
